4a9650d09f07ebde67dc5f174ca8cd5fc705cb90
Davide Bertolino <davide.bertolino@ecat.it>
2017-10-30 10:51:05 +0100

    First Commit on GIT

---
+++ StreamingApp/ApplicationEvents.vb
@@ -0,0 +1,22 @@
+﻿Namespace My
+    ' I seguenti eventi sono disponibili per MyApplication:
+    ' 
+    ' Startup: generato all'avvio dell'applicazione, prima della creazione del form di avvio.
+    ' Shutdown: generato dopo la chiusura di tutti i form dell'applicazione. L'evento non è generato se l'applicazione termina in modo anormale.
+    ' UnhandledException: generato se l'applicazione rileva un'eccezione non gestita.
+    ' StartupNextInstance: generato quando si avvia un'applicazione istanza singola e l'applicazione è già attiva. 
+    ' NetworkAvailabilityChanged: generato quando la connessione di rete è connessa o disconnessa.
+    Partial Friend Class MyApplication
+
+        Private Sub MyApplication_Startup(sender As Object, e As ApplicationServices.StartupEventArgs) Handles Me.Startup
+            'Dim iMonitorCount As Integer = Screen.AllScreens.Length
+            'If iMonitorCount > 1 Then
+            'Else
+
+            'End If
+        End Sub
+    End Class
+
+
+End Namespace
+
---
+++ StreamingApp/Form1.vb
@@ -0,0 +1,115 @@
+﻿Imports System.Runtime.InteropServices
+
+
+Public Class Form1
+
+    <DllImport("user32.dll", SetLastError:=True)> _
+    Private Shared Function SetWindowPos(ByVal hWnd As IntPtr, ByVal hWndInsertAfter As IntPtr, ByVal X As Integer, ByVal Y As Integer, ByVal cx As Integer, ByVal cy As Integer, ByVal uFlags As SetWindowPosFlags) As Boolean
+    End Function
+    Private ReadOnly HWND_BOTTOM As New IntPtr(1)
+    Private ReadOnly HWND_NOTOPMOST As New IntPtr(-2)
+    Private ReadOnly HWND_TOP As New IntPtr(0)
+    Private ReadOnly HWND_TOPMOST As New IntPtr(-1)
+
+    Public Const SWP_NOMOVE = &H2
+    Public Const SWP_NOSIZE = &H1
+    Public Const SWP_NOACTIVATE = &H10
+    Public Const SWP_SHOWWINDOW = &H40
+    Public Const SWP_NOREDRAW = &H8
+
+    <Flags> _
+    Private Enum SetWindowPosFlags As UInteger
+        ''' <summary>If the calling thread and the thread that owns the window are attached to different input queues, 
+        ''' the system posts the request to the thread that owns the window. This prevents the calling thread from 
+        ''' blocking its execution while other threads process the request.</summary>
+        ''' <remarks>SWP_ASYNCWINDOWPOS</remarks>
+        SynchronousWindowPosition = &H4000
+        ''' <summary>Prevents generation of the WM_SYNCPAINT message.</summary>
+        ''' <remarks>SWP_DEFERERASE</remarks>
+        DeferErase = &H2000
+        ''' <summary>Draws a frame (defined in the window's class description) around the window.</summary>
+        ''' <remarks>SWP_DRAWFRAME</remarks>
+        DrawFrame = &H20
+        ''' <summary>Applies new frame styles set using the SetWindowLong function. Sends a WM_NCCALCSIZE message to 
+        ''' the window, even if the window's size is not being changed. If this flag is not specified, WM_NCCALCSIZE 
+        ''' is sent only when the window's size is being changed.</summary>
+        ''' <remarks>SWP_FRAMECHANGED</remarks>
+        FrameChanged = &H20
+        ''' <summary>Hides the window.</summary>
+        ''' <remarks>SWP_HIDEWINDOW</remarks>
+        HideWindow = &H80
+        ''' <summary>Does not activate the window. If this flag is not set, the window is activated and moved to the 
+        ''' top of either the topmost or non-topmost group (depending on the setting of the hWndInsertAfter 
+        ''' parameter).</summary>
+        ''' <remarks>SWP_NOACTIVATE</remarks>
+        DoNotActivate = &H10
+        ''' <summary>Discards the entire contents of the client area. If this flag is not specified, the valid 
+        ''' contents of the client area are saved and copied back into the client area after the window is sized or 
+        ''' repositioned.</summary>
+        ''' <remarks>SWP_NOCOPYBITS</remarks>
+        DoNotCopyBits = &H100
+        ''' <summary>Retains the current position (ignores X and Y parameters).</summary>
+        ''' <remarks>SWP_NOMOVE</remarks>
+        IgnoreMove = &H2
+        ''' <summary>Does not change the owner window's position in the Z order.</summary>
+        ''' <remarks>SWP_NOOWNERZORDER</remarks>
+        DoNotChangeOwnerZOrder = &H200
+        ''' <summary>Does not redraw changes. If this flag is set, no repainting of any kind occurs. This applies to 
+        ''' the client area, the nonclient area (including the title bar and scroll bars), and any part of the parent 
+        ''' window uncovered as a result of the window being moved. When this flag is set, the application must 
+        ''' explicitly invalidate or redraw any parts of the window and parent window that need redrawing.</summary>
+        ''' <remarks>SWP_NOREDRAW</remarks>
+        DoNotRedraw = &H8
+        ''' <summary>Same as the SWP_NOOWNERZORDER flag.</summary>
+        ''' <remarks>SWP_NOREPOSITION</remarks>
+        DoNotReposition = &H200
+        ''' <summary>Prevents the window from receiving the WM_WINDOWPOSCHANGING message.</summary>
+        ''' <remarks>SWP_NOSENDCHANGING</remarks>
+        DoNotSendChangingEvent = &H400
+        ''' <summary>Retains the current size (ignores the cx and cy parameters).</summary>
+        ''' <remarks>SWP_NOSIZE</remarks>
+        IgnoreResize = &H1
+        ''' <summary>Retains the current Z order (ignores the hWndInsertAfter parameter).</summary>
+        ''' <remarks>SWP_NOZORDER</remarks>
+        IgnoreZOrder = &H4
+        ''' <summary>Displays the window.</summary>
+        ''' <remarks>SWP_SHOWWINDOW</remarks>
+        ShowWindow = &H40
+    End Enum
+
+    Private Function GetIPv4Address() As String
+        GetIPv4Address = String.Empty
+        Dim strHostName As String = System.Net.Dns.GetHostName()
+        Dim iphe As System.Net.IPHostEntry = System.Net.Dns.GetHostEntry(strHostName)
+
+        For Each ipheal As System.Net.IPAddress In iphe.AddressList
+            If ipheal.AddressFamily = System.Net.Sockets.AddressFamily.InterNetwork Then
+                GetIPv4Address = ipheal.ToString()
+            End If
+        Next
+
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim rectmonitor As Rectangle
+        Dim iMonitorCount As Integer = Screen.AllScreens.Length
+        If iMonitorCount > 1 Then
+            rectmonitor = Screen.AllScreens(1).WorkingArea
+        Else
+            rectmonitor = Screen.AllScreens(0).WorkingArea
+        End If
+        SetWindowPos(Me.Handle, 0, rectmonitor.Left, rectmonitor.Top, rectmonitor.Width, rectmonitor.Height, SWP_SHOWWINDOW)
+        Dim u As New Uri("http://video.virginradioitaly.it/com/universalmind/swf/video_player_102.swf?xmlPath=http://video.virginradioitaly.it/com/universalmind/tv/virgin/videoXML.xml&advXML=http://video.virginradioitaly.it/com/universalmind/adsWizzConfig/1.xml")
+        Me.WebBrowser1.Url = u
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        Dim u As New Uri("http://www.105.net/upload/com/universalmind/swf/video_player_102.swf?xmlPath=/upload/com/universalmind/tv/105/videoXML.xml&advXML=/upload/com/universalmind/adsWizzConfig/0.xml")
+        Me.WebBrowser1.Url = u
+    End Sub
+
+End Class
